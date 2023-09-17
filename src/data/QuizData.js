e82629e419c868b5b1b50f1b09fbae38aece9024
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2023-09-18 01:18:13 +0530

    first commit

---
+++ src/data/QuizData.js
@@ -0,0 +1,43 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question:"Which of the following is NOT a commonly used version control system",
+        options:["Git"," Subversion (SVN)"," Mercurial","PHP"],
+        answer:4
+
+    },
+    {
+        question:"What is the role of MongoDB in the MERN stack",
+        options:["Rendering user interfaces","Handling server-side logic","Managing the database","Controlling application flow"],
+        answer:3
+    },
+    {
+        question:"Which JavaScript library is commonly used for building user interfaces in the MERN stack",
+        options:["Angular","Vue.js","React","Ember.js"],
+        answer:3
+    },
+    {
+        question:"Which MERN stack component is responsible for handling asynchronous operations and I/O tasks",
+        options:[" Express.js","MongoDB","React","Node.js"],
+        answer:4
+    }
+];
